bb25ad10f056360cd4282ee6f7828a87b13919c2
JCornelison <gitter@VashonDesign.com>
2024-10-24 11:09:35 -0700

    Initial commit, with a few known issues still

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # ArcTree
-Create nicely formatted unordered list with arcs from JSON files with expand/collapse options &amp; variable font size.
+
+> Create nicely formatted unordered list with arcs from JSON files with
+> expand/collapse options &amp; variable font size.
+
+![ArcTree](ArcTree.png)
+
+## Introduction to ArcTree
+
+This HTML + Javascript page will recurse through the file and build an unordered
+HTML tree with nice arcs and collapsible nodes.
+
+## ArcTree Options
+
+There are also some options at the top (if shown the the user) that allows
+collapsing or expanding the whole tree:
+![Options Dialog for ArcTree](ArcTreeOptions.png)
+
+- **Collapse Leafs** will temporarily hide list nodes which have a blank
+  children node, i.e., `children:[]`. If the `children` node is missing, things
+  work, but they aren't considered leafs & won't be collapsed.
+- **Show Leafs** just re-expands all nodes if any were hidden.
+- **Collapse Tree** will collapse _all_ nodes in the entire tree, allowing users
+  to gradually expand only the nodes they wish.
+- **Expand Tree** expands (opens) all nodes in the tree, allowing users to
+  selectively collapse any nodes they don't wish taking up visible space
+- **Font Size** changes the font size between 0.5 on up to 5.0 in 0.5
+  increments.
+- **Browse** button allows selecting a JSON file formatted as shown above. Upon
+  being selected, any existing tree is cleared and rebuilt.
+- **Relative URLs** allows users to express whether their json file provides
+  relative/incremental URLs or full/complete URLs. If changed, any existing tree
+  is cleared and if a file has previously been selected, the tree is rebuilt.
+
+## Formatting
+
+Although not trivial, folks that know basic CSS can alter the symbols and colors
+used to display open and closed nodes. I have tried to provide more
+documentation as to what CSS affects various parts of the ArcTree!
+
+## JSON Data File Format
+
+JSON files are expected to be formatted as:
+
+```JSON
+{
+    "title": "web page title",
+    "url": "web page url",
+    "meta": "web page description",
+    "children": [
+        {
+            "title": "child page title",
+            "url": "child page url",
+            "meta": "child page description",
+            "children": [{...}, {...}]
+        },
+        {
+            "title": "child page title",
+            "url": "child page url",
+            "meta": "child page description",
+            "children": []
+        }
+    ]
+}
+```
+
+- URLs can be relative/incremental to their parent or specified completely, as
+  noted in the Options section above.
+- BUG: Currently the base URL is hardcoded in the `arc-tree.js` file! Search for
+  `baseUrl = "https:\\fema.gov"` in the function `fileChange()` & manually
+  change this for your situation.
+- If the `children` node is missing, the tree will be built, but they won't be
+  considered leafs & won't be collapsed if the user clicks the _Collapse Leafs_
+  button.
+- Additional attributes (akin to "title", "meta", or "url") can be included for
+  each node, but will be denoted with "[Unknown node (" in the tree display.
+  Adding new cases to the `switch` statement in `function buildArcTree` in the
+  `arc-tree.js` file should be pretty trivial though.
+- When opened, JSON files are validated just by seeing they have success with
+  JSON.parse currently. In the future we may enable AJX validation which is more
+  sophisticated. Read more about it at: https://www.npmjs.com/package/ajv.
+
+## Debugging
+
+- By default, the `arc-tree.js` file has `let verbose = false;` at the top.
+  Change this to `true` if you wish to track output and see errors, warnings and
+  verbose information. These can be viewed by opening any browser's developers
+  tools(F12 by convention) and then it's 'console window'.
+
+## License & Copyright
+
+This is released under the permissive MIT License. For details see
+![License](LICENSE)
+
+Thanks to https://codepen.io/bisserof/pen/nrMveb for the nice formatting of the
+arcs! Their original sample provided the inspiration for this project.
+
+©2024, https://eoc.online
+
+Credit for reuse of this is always appreciated!
---
+++ arc-tree.js
@@ -0,0 +1,309 @@
+// or ESM/TypeScript import
+//import Ajv from "ajv"
+let verbose = true;
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (verbose) console.clear();
+  //if (verbose) console.log("DOM fully loaded and parsed");
+  setFontSize(document.querySelector("#fontSize"));
+  document.getElementById("arcTreeFile").value = "";
+});
+
+
+/// 'Tree Options' functionality =====================
+function setFontSize(el) {
+  let fontSize = el.value;
+  if (!isNumber(fontSize) || fontSize < 0.5 || fontSize > 5) {
+    fontSize = 1;
+  }
+  document.querySelector(".tree").style.fontSize = fontSize + 'em';
+  document.querySelector("#fontSizeValue").innerHTML = fontSize;
+  document.querySelector("#fontSize").value = fontSize;
+}
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function collapseLeafs() {
+  var leafs = document.getElementsByClassName("leaf");
+  var leafsLen = leafs.length;
+  for (var i = 0; i < leafsLen; i++) {
+    leafs[i].style.display = "none";
+  }
+  if (verbose) console.log("Collapsed all " + leafsLen + " leafs.");
+}
+
+function showLeafs() {
+  var leafs = document.getElementsByClassName("leaf");
+  var leafsLen = leafs.length;
+  for (var i = 0; i < leafsLen; i++) {
+    leafs[i].style.display = "block";
+  }
+  if (verbose) console.log("Displayed all " + leafsLen + " leafs.");
+}
+
+function collapseTree() {
+  var checkboxes = document.querySelector(".tree").getElementsByTagName("input");
+  var len = checkboxes.length;
+  for (var i = 0; i < len; i++) {
+    checkboxes[i].checked = "";
+  }
+  if (verbose) console.log("Collapsed all " + checkboxes.length + " nodes.");
+}
+
+function expandTree() {
+  var checkboxes = document.querySelector(".tree").getElementsByTagName("input");
+  var len = checkboxes.length;
+  for (var i = 0; i < len; i++) {
+    checkboxes[i].checked = "checked";
+  }
+  if (verbose) console.log("Expanded all " + checkboxes.length + " nodes.");
+}
+
+
+
+/// Import & Validate Json functionality =====================
+async function readJSONFile(file) {
+  return new Promise((resolve, reject) => {
+    let fileReader = new FileReader();
+    fileReader.onload = event => {
+      resolve(JSON.parse(event.target.result))
+    };
+    fileReader.onerror = (error => reject(error));
+    fileReader.readAsText(file);
+  });
+}
+
+async function fileChange(file) {
+  readJSONFile(file).then(
+    json => {
+      if (verbose) console.log("Read new Json file: " + file.name);
+      //var baseUrl = new URL(file.name, window.location.href).href;
+      // TODO: get base URL from base element in JSON file
+
+      var baseUrl = "https:\\fema.gov"
+      validateJson(json);
+      clearArcTree(document.getElementById('unorderedArcTree'));
+      buildArcTree(json, document.getElementById('unorderedArcTree'), baseUrl);
+    }
+  );
+}
+
+function validateJson(json) {
+  try {
+    JSON.parse(json);
+  } catch (e) {
+    if (verbose) console.error("Invalid JSON: " + e);
+    return false;
+  }
+  if (verbose) console.log("json validated using JSON.parse")
+  return true;
+}
+
+
+// https://ajv.js.org/guide/getting-started.html#basic-data-validation
+// https://www.npmjs.com/package/ajv
+
+// Node.js require:
+/*
+const Ajv = require("ajv")
+const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+
+const schema = {
+  type: "object",
+  properties: {
+    foo: { type: "integer" },
+    bar: { type: "string" },
+  },
+  required: ["foo"],
+  additionalProperties: false,
+}
+
+const schema2 = {
+  type: "object",
+  properties: {
+    title: { type: "string" },
+    url: { type: "string" },
+    meta: { type: "string" },
+    children: { type: "object" },
+  },
+  required: ["title"],
+  additionalProperties: false,
+}
+
+const data = {
+  foo: 1,
+  bar: "abc",
+}
+
+const data2 = {
+  title: "test title",
+  url: "test url",
+  meta: "test meta",
+  children: {
+    title: "test title",
+    url: "test url",
+    meta: "test meta",
+    children: {
+      title: "test title",
+      url: "test url",
+      meta: "test meta",
+    }
+  }
+}
+
+const validate = ajv.compile(schema)
+const valid = validate(data)
+if (!valid) {
+  if (verbose) console.error(validate.errors)
+} else {
+  if (verbose) console.log("Json validated using Ajv")
+}
+
+/// Unused!
+function downloadJson() {
+  var json = document.getElementById('unorderedArcTree').innerHTML;
+  var blob = new Blob([json], { type: "text/plain;charset=utf-8" });
+  saveAs(blob, "arcTree.json");
+}
+
+/// Unused!
+function uploadJson() {
+  var file = document.getElementById('arcTreeFile').files[0];
+  if (file) {
+    fileChange(file);
+  }
+}
+*/
+
+
+/// Tree Creation functionality =====================
+var expandedByDefault = true;
+var relativeUrls = true; // If true, append child URLs to parent URL
+var listItemHTML = "";
+var listLog = "";
+
+function clearArcTree(treeElement) {
+  treeElement.innerHTML = '';
+}
+
+/// Allow for JSON objects with partial or full URLs
+function relativeUrls() {
+  relativeUrls = !relativeUrls;
+  if (verbose) console.log("cumulativeUrls: " + relativeUrls);
+  document.getElementById('arcTreeUrl').checked = relativeUrls;
+  document.getElementById('arcTreeUrlSpan').innerHTML = relativeUrls ? "Relative URLs" : "Full URLs";
+  clearArcTree(document.getElementById('unorderedArcTree'));
+  fileChange(document.getElementById('arcTreeFile').files[0]);
+}
+
+/// <summary>
+/// Recurse through an 'o' JSON object & build up an HTML unordered list
+/// Parameters:
+/// 'o' is the JSON object to be processed, of the following form:
+/***
+      o = {
+        "title": "title of web page",
+        "url": "relative url of web page",
+        "meta": "web page description",
+        "children": [{
+          "title": "title of child page",
+          "url": "relative url of child page",
+          "meta": "child page description",
+          "children": [{...}, {...}]
+        }]
+      }
+***/
+/// 'treeElement' is the HTML element to which the unordered list is to be appended
+/// 'url' is this this segment of the tree's full or cumulative (N.B., see above option) url.
+///   Initially url is just the base/home URL.
+/// </summary>
+
+function buildArcTree(o, treeElement, url) {
+  //if (verbose) console.log("buildArcTree: " + o?.toString());
+  for (var i in o) {
+    // if (verbose) console.log("processing: " + i.toString());
+    if (o[i] instanceof Array) {
+      // if (verbose) console.log("got Array");
+      // treeElement.appendChild(document.createComment(i + ": ARRAY"));
+    }
+    else if (o[i] instanceof Object) {
+      // if (verbose) console.log("got Object");
+      // treeElement.appendChild(document.createComment(i + ": OBJECT"));
+    }
+    else {
+      //if (verbose) console.log(i + ': ' + o[i]);
+      listLog += i + '=' + o[i] + ";  ";
+
+      switch (i) {
+        // Add other JSON nodes below...
+        case "title": listItemHTML += "<b>" + o[i] + "</b>"; break;
+        case "url":
+          if (relativeUrls) {
+            childUrl = url + o[i];
+          } else {
+            childUrl = o[i];
+          }
+          listItemHTML += " (<a href='" + url + "'>" + o[i] + "</a>): ";
+          break;
+        case "meta": listItemHTML += "<i> Level " + o[i] + "</i>"; break;
+        default: listItemHTML += " [Unknown node (" + i + ")=" + o[i] + "] ";
+      }
+    }
+
+    if (o[i] instanceof Object) {
+
+      if (listItemHTML != "") {
+        // if (verbose) console.log("=== dump caches!");
+        // Output list item we've been building up before processing children
+        if (verbose) console.log(listLog);
+        listLog = "";
+
+        var uniqueID = Math.floor(Math.random() * 1000000).toString();
+        var newLI = document.createElement('li');
+
+        var newInput = document.createElement('input');
+        newInput.id = "c" + uniqueID;
+        newInput.type = "checkbox";
+        newInput.checked = expandedByDefault;
+
+        var newLabel = document.createElement('label');
+        newLabel.htmlFor = "c" + uniqueID;
+        newLabel.className = "tree_label";
+        newLabel.innerHTML = DOMPurify.sanitize(listItemHTML);  //NOTE: Sanitization is not needed with trusted JSON
+
+        // Or if last leaf (no children), add a leaf class
+        if (Object.keys(o[i]).length == 0) {
+          newLI.className = "leaf";
+          var newSpan = document.createElement('span');
+          newSpan.className = "tree_label";
+          newSpan.innerHTML = DOMPurify.sanitize(listItemHTML);  //NOTE: Sanitization is not needed with trusted JSON
+          newLI.appendChild(newSpan);
+        }
+        listItemHTML = "";
+        treeElement.appendChild(newLI);
+
+        if (Object.keys(o[i]).length > 0) {
+          newLI.appendChild(newInput);
+          newLI.appendChild(newLabel);
+        }
+        treeElement = newLI;
+      }
+
+      var newUL = treeElement;
+      if (o[i] instanceof Array) {
+        newUL = document.createElement('ul');
+        // newUL.className = "array";
+        treeElement.appendChild(newUL);
+      } else if (o[i] instanceof Object) {
+        // no need to create a new UL for non-arrays
+        //newUL = treeElement;
+      }
+
+      if (verbose) console.group("children of " + i);
+      buildArcTree(o[i], newUL, childUrl);
+      if (verbose) console.groupEnd();
+    }
+  }
+}
